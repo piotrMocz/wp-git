576a2b8428dcd37b662b891d0e0be5184546e7df
piotrMocz <piotr.moczurad@gmail.com>
2021-11-09 12:45:09 +0100

    Add A and B

---
+++ A.py
@@ -0,0 +1,8 @@
+def f(x, y):
+    return x**y
+
+
+
+def g(x):
+    return x**2
+
---
+++ B.py
@@ -0,0 +1,12 @@
+
+def pow(x, y):
+    return x**y
+
+
+def foo():
+    return 'siema'
+
+
+def g(x):
+    return x**2
+
